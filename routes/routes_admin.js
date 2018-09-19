67129d60c18d8e1da8d413bef6e9306d5c36203d
Fadlurss <fadlurss@gmail.com>
2018-09-19 16:18:04 +0700

    init

--- routes/routes_admin.js
+++ routes/routes_admin.js
@@ -3,13 +3,35 @@ var  express = require('express')
      Campgrounds = require('../models/campground');
      Comments = require('../models/comment');
      Users = require('../models/user');
+     middleware = require("../middleware");
 
-     router.get("/", (req,res)=>{
-     	Campgrounds.find({author: req.user.id}).populate("comments","user").sort({created: -1}).exec(function(err, campgrounds){
+     router.get("/", middleware.isLoggedIn , (req,res)=>{
+     	Campgrounds.find({}).populate("comments").sort({created: -1}).exec(function(err, campgrounds){
             res.render("v_admin/index", {campgrounds : campgrounds});
             console.log();
          });
+        // Campgrounds.find({author: req.user.id})
+        //     .populate('comments')
+        //     .then(campgrounds=>{
+    
+        //         res.render("v_admin/index", {campgrounds : campgrounds});
+        //     });
+    
      });
+
+     router.get('/:id', (req, res)=>{
+
+
+        Campgrounds.find({author: req.user.id})
+            .populate('comments')
+            .then(posts=>{
+    
+                res.render("v_admin/index", {campgrounds : campgrounds});
+            });
+    
+    
+    
+    });
 
      router.get("/comments", (req,res)=>{
         Comments.find({}).populate("user").sort({created: -1}).exec(function(err, comments){

384379f5ee00c6b2b9914008b1ff410511926a9e
Fadlurss <fadlurss@gmail.com>
2018-09-28 10:29:59 +0700

    init

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'        : '315003429251209', // your App ID
-        'clientSecret'    : 'e43b6f50eed0fe2c0df60ca35c8649e1', // your App Secret
+        'clientID'        : '', // your App ID
+        'clientSecret'    : '', // your App Secret
         'callbackURL'     : 'http://localhost:2000/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields'   : ['id', 'email', 'name'] // For requesting permissions from Facebook API
@@ -19,8 +19,8 @@ module.exports = {
     },
 
     'googleAuth' : {
-        'clientID'         : '957405754426-0rv1m8nuebmcpgcbjqsops6rg4rbatad.apps.googleusercontent.com',
-        'clientSecret'     : 'XMFAIvNRJWp2pN7HToHv9sx4',
+        'clientID'         : '',
+        'clientSecret'     : '',
         'callbackURL'      : 'http://localhost:5000/auth/google/callback'
     }
 
